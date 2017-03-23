09956c34c753da5c5031e9e1e7c51e55b7c77803
Saarule <saaryazdi@gmail.com>
2017-03-23 13:41:44 +0200

    Commit

--- MarketClient/DataEntries/IMarketUserData.cs
+++ MarketClient/DataEntries/IMarketUserData.cs
@@ -8,6 +8,7 @@ namespace MarketClient.DataEntries
 {
     public class IMarketUserData
     {
+        //אהלן אחי
         Dictionary<string, int> Commodities { get; set; }
         int Funds { get; set; }
         LinkedList<int> Requests { get; set; }
