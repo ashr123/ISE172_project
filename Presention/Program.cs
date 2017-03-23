817931f226711c13f6fcac3adab406be9867229d
NoySaban <noysaba@post.bgu.ac.il>
2017-03-24 00:24:08 +0200

    n

---
+++ Presention/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presention
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, This is an Algo-trading market system.\n We Support the following operations: ");
+            Console.WriteLine("Press 1 to open a new request (buy/sell)\n Press 2 to cancel an exist request\n Press 3 to ask a query \n Press 4 to");
+            String input = Console.ReadLine();
+
+            if (input.Length!=1)
+            {
+                Console.WriteLine("You entered illegal input.");
+                Console.WriteLine("Press 1 to open a new request (buy/sell)\n Press 2 to cancel an exist request\n Press 3 to ask a query \n Press 4 to");
+            }//if
+
+            else
+            {
+                
+                    }
+        }
+    }
+}
