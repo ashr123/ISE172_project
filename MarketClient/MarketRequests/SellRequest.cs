1f8fe65cadf9b98e9074d572df565d1c922e239a
רועי אש <ashr@post.bgu.ac.il>
2017-03-23 13:34:03 +0200

    GGGG

---
+++ MarketClient/MarketRequests/SellRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketClient.Market_Requests
+{
+    class SellRequest
+    {
+        int Commodity { get; set; }
+        int Amount { get; set; }
+        int Price { get; set; }
+    }
+}
