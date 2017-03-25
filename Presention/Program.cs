e76b04bde0bf89e39d7c20c9a65e4f4c176c5467
NoySaban <noysaba@post.bgu.ac.il>
2017-03-26 01:51:09 +0300

    almost done - presetion

--- Presention/Program.cs
+++ Presention/Program.cs
@@ -9,143 +9,156 @@ namespace Presention
 {
     public class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, This is an Algo-trading market system.\n We Support the following operations: ");
             Console.WriteLine("Press 1 to open a new request (buy/sell)\n Press 2 to cancel an exist request\n Press 3 to ask a query \n");
-            String input=Console.ReadLine();
+            String input = Console.ReadLine();
 
-            bool isLegal=CheckLegality(input, 3);
+            bool isLegal = CheckLegality(input, 3);
             while (isLegal == false)
                 PrintWhenIllegalkey();
-
-
-            if (input.Equals('1'))
+            switch (input)
             {
-                Console.WriteLine("You chose to open a new request./n To buy press 1/n To sell press 2");
-                input=Console.ReadLine();
+                case "1":
+                    Console.WriteLine("You chose to open a new request./n To buy press 1/n To sell press 2");
+                    string input1 = Console.ReadLine();
+                    CollectingInfoBUYSELL(input1);
+                    break;
 
-                isLegal=CheckLegality(input, 2);
-                while (isLegal == false)
-                {
-                    PrintWhenIllegalkey();
-                    input = Console.ReadLine();
-                    isLegal = CheckLegality(input, 2);
-                }
+                case "2":
+                    Console.WriteLine("You chose to cancel a request./n");
+                    CollectInfoCancelRequst();
+                    break;
 
-                CollectingInfoBUYSELL(input);
-            }
+                case "3":
+                    Console.WriteLine("You chose to ask a query./n To buy/sellQuery press 1/n To userQuery press 2/n To marketQuery press 3 ");
+                    string input3 = Console.ReadLine();
+                    CollectInfoQueryRequst(input);
+                    break;
 
-            if (input.Equals('2'))
+                default:
+                    
+                    break;
+            }//switch
+
+        }//main
+
+
+        private static void IsLegalCombinedLoop(String s, int options)
+        {
+            bool isLegal = CheckLegality(s, options);
+            while (isLegal == false)
             {
-                Console.WriteLine("You chose to cancel a request./n");
-
-                CollectInfoCancelRequst();
+                PrintWhenIllegalkey();
+                s = Console.ReadLine();
+                isLegal = CheckLegality(s, options);
             }
-
-
-            if (input.Equals('3'))
-            {
-                Console.WriteLine("You chose to ask a query./n To buy/sellQuery press 1/n To userQuery press 2/n To marketQuery press 3 ");
-                input=Console.ReadLine();
-
-                isLegal=CheckLegality(input, 3);
-                while (isLegal == false)
-                    PrintWhenIllegalkey();
-
-                CollectInfoQueryRequst(input);
-            }
-
+            return;
         }
-
-        private static bool CheckLegality(String s, int options) {
+        private static bool CheckLegality(String s, int options)
+        {
 
             if (s.Length != 1)
                 return false;
-            for(int i=1; i <= options; i++)
+            for (int i = 1; i <= options; i++)
                 if (s.Equals('i'))
                     return true;
 
             return false;
         }
-        
+
         private static void PrintWhenIllegalkey()
         {
-            Console.WriteLine("You entered illegal input. Please press a llegal key.");
+            Console.WriteLine("You entered illegal input. Please press a legal key.");
+            return;
         }
 
         private static void CollectingInfoBUYSELL(string a)
         {
+            //write more legallity checks
             Console.WriteLine("Please enter Commodity");
-            int commodity=Convert.ToInt32(Console.ReadLine());
+            int Commodity = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Please enter Amount");
-            int amount = Convert.ToInt32(Console.ReadLine());
+            int Amount = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Please enter Price");
-            int price = Convert.ToInt32(Console.ReadLine());
+            int Price = Convert.ToInt32(Console.ReadLine());
 
+            MarketClientClass client = new MarketClientClass(); //create client to use it's methoods
 
             //write more legallity checks
-
-            if (a.Equals('1')) {
-                BuyRequest buy=new BuyRequest;
-                buy.Commodity=commodity;
-                buy.Amount=amount;
-                buy.Price=price;
+            int IDbuysell;
+            if (a.Equals('1'))
+            {
+                IDbuysell = client.SendBuyRequest(Price, Commodity, Amount);
             }
             else
             {
-                SellRequest sell=new SellRequest;
-                sell.Commodity=commodity;
-                sell.Amount=amount;
-                sell.Price=price;
+                IDbuysell = client.SendSellRequest(Price, Commodity, Amount);
             }
 
-            //print if well
-        }
+            Console.WriteLine("The request done successfully. The ID is " + IDbuysell);
+            //print Id if well
+
+
+            //add what if not succeed?
+            return;
+        }//collect info buy-sell request
 
         private static void CollectInfoCancelRequst()
         {
             Console.WriteLine("Please enter the ID request you wish to cancel./n");
-            int id=Console.ReadKey();
+            int id = Console.ReadKey();
 
-            CancelBuySellRequest cancel=new CancelBuySellRequest;
-            cancel.Id=id;
-            //print if well
 
-        }
+            MarketClientClass client = new MarketClientClass(); //create client to use it's methoods
+
+            bool ans = client.SendCancelBuySellRequest(id);
+
+            if (ans == true)
+                Console.WriteLine("Cancellation succeed./n");
+            else
+                Console.WriteLine("Cancellation failed./n");
+            //print answer
+
+            return;
+        }//collect info cancel request
 
         private static void CollectInfoQueryRequst(string a)
         {
-            if (a.Equals('1'))
+
+            MarketClientClass client = new MarketClientClass(); //create client to use it's methoods
+
+
+            switch (a)
             {
-                Console.WriteLine("Please enter the ID request./n");
-                int id=Console.ReadKey();
+                case "1":
+                    Console.WriteLine("Please enter the ID request./n");
+                    int id = Convert.ToInt32(Console.ReadLine());
 
-                QueryBuySellRequest query=new QueryBuySellRequest;
-                query.Id=id;
+                    MarketItemQuery data1 = client.SendQueryBuySellRequest(id);
+                    Console.WriteLine("Buy-Sell query info is:/n" + data1.ToString());  //print data on a certain deal
 
-                //print on a certain deal
-            }
+                    break;
 
+                case "2":
+                    MarketUserData data2 = client.SendQueryUserRequest;
 
-            if (a.Equals('2'))
-            {
-                QueryUserRequest query=new QueryUserRequest;
-                //print user inf
-            }
+                    Console.WriteLine("User query info is:/n" + data2.ToString());
+                    break;
 
-            if (a.Equals('3'))
-            {
-                Console.WriteLine("Please enter the Commodity./n");
-                int commodity=Console.ReadKey();
+                case "3":
+                    Console.WriteLine("Please enter the Commodity./n");
+                    int commodity = Convert.ToInt32(Console.ReadLine());
 
-                QueryMarketRequest query=new QueryMarketRequest;
-                query.Commodity=commodity;
+                    MarketCommodityOffer data3 = client.SendQueryMarketRequest(commodity);
+                    Console.WriteLine("Market query info is:/n" + data3.ToString());
+                    break;
 
-                //print comm inf
+            }//switch
+            return;
+        }//collect info query
 
-            }
-
-        }
-    }
-}
+    }//class 
+}//namespace presention
