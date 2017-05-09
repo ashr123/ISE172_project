cff9ba3f78643a8f64bd5cffec068fefd028ca30
רועי אש <ashr@post.bgu.ac.il>
2017-05-09 22:50:42 +0300

    First Commit

---
+++ MarketClient/Class1.cs
@@ -0,0 +1,21 @@
+﻿using log4net;
+using DataTier.Loggers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTier
+{
+    class Class1
+    {
+		public static ILog myLogger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+		static void Main(string[] args)
+        {
+			HistoryLogger.WriteHistory("fffffffffffffffff");
+			//myLogger.Debug("asdasdasdasdasd");
+			Console.ReadLine();
+        }
+    }
+}
---
+++ MarketClient/Loggers/HistoryLogger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using log4net;
+
+namespace DataTier.Loggers
+{
+	class HistoryLogger
+	{
+		//static ILog myLogger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+		private static readonly ILog myLogger = LogManager.GetLogger("History");
+		public static void WriteHistory(string contents)
+		{
+			//File.AppendAllText(@"..\..\..\Log\history.txt", DateTime.Now+","+contents+"\n");
+			myLogger.Info(contents);
+		}
+
+		public static string[] ReadHistory()
+		{
+			return File.ReadAllLines(@"..\..\..\Log\history.txt");
+		}
+	}
+}
