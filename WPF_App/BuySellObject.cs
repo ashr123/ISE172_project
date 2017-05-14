0b04889aef773d925d02a28c80c442a171ac2aa3
Saarule <saaryazdi@gmail.com>
2017-05-14 19:48:54 +0300

    gui

---
+++ WPF_App/BuySellObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_App
+{
+    public enum Operation { Buy, Sell }
+    public class BuySellObject
+    {
+        private Operation operation;
+        private int Price { get; set; }
+        private int Commodity { get; set; }
+        private int Amount { get; set; }
+
+        public BuySellObject(Operation operation, int Price, int Commodity, int Amount)
+        {
+            this.operation = operation;
+            this.Price = Price;
+            this.Commodity = Commodity;
+            this.Amount = Amount;
+
+        } 
+    }
+}
---
+++ WPF_App/SellPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_App
+{
+    /// <summary>
+    /// Interaction logic for SellPage.xaml
+    /// </summary>
+    public partial class SellPage : Page
+    {
+        public SellPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int Commodity;
+            int Price;
+            int Amount;
+
+            if (!(Int32.TryParse(CommodityField.Text, out Commodity)))
+                MessageBox.Show("Invalid Commodity", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Error);
+            if (!(Int32.TryParse(PriceField.Text, out Price)))
+                MessageBox.Show("Invalid Price", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Error);
+            if (!(Int32.TryParse(AmountField.Text, out Amount)))
+                MessageBox.Show("Invalid Amount", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Error);
+            if (Commodity > 0 && Price > 0 && Amount > 0)
+            {
+                BuySellObject myObject = new BuySellObject(Operation.Buy, Commodity, Price, Amount);
+                //Send the object here..
+            }
+            else
+            {
+                MessageBox.Show("Invalid Input", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+    }
+}
