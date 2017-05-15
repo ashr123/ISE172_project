3f9710d621438573887f81e9745cadb01a123403
NoySaban <noysaba@post.bgu.ac.il>
2017-05-15 22:10:21 +0300

    AMA changes+ logging + timer - not done

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -5,73 +5,115 @@ using System.Text;
 using System.Threading.Tasks;
 using DataTier;
 using DataTier.DataEntries;
-
+using System.Timers;
+using DataTier.Loggers;
 
 namespace LogicTier
 
 {
     class Program
     {
-        static void Main(string[] args)
+        
+        public static void TimerOfAMA(bool b)
         {
+            Timer myTimer = new Timer(2000);
+            myTimer.Elapsed += new ElapsedEventHandler(onTimedEvent);
 
+            if (b)
+                myTimer.Start();
+            
 
-            //here we'll call AMA with commoodity ect. can use an algorithem to choose what to buy
-
-
-
-            //note: AMA calling the server few times- maybe need another timer? so won't call to fast
-
-            System.Timers.Timer myTimer = new System.Timers.Timer();
-
-            myTimer.Interval = 2000;
-            myTimer.Start();
-
-            myTimer.Stop();
-
-
-            myTimer.Tick += new EventHandler(TimerEventProcessor);
-
-            myTimer.
-            private static void myTimer_Tick(object sender, EventArgs e)
-        {
+            else
+                myTimer.Stop();
 
         }
+        
+
+            private static void onTimedEvent(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(0, 10);
+            AMA_Buy(num, 2, 8);
+
+
+            //ama sell
+        }
+
+        private static void wasteTime(object sender, EventArgs e)
+        {
+
+                      //nothing
+            return;
+
+        }
+
+
+
+        public static void AMA_Buy(int commodity, int desiredPrice, int amount)
+        {
+            int counter = 0;
+            MarketClientClass client = new MarketClientClass();
+            AllMarketRequest all = client.QueryAllMarketRequest();
+            counter++;
+
+
+            foreach (ItemAskBid item in all.MarketInfo)
+                if (item.Id == commodity && item.Info.Ask <= desiredPrice)
+                {   //if item is the right commodity & right price
+
+                    MarketUserData userData = client.SendQueryUserRequest();
+                    counter++;
+
+                    Timer timeWaster = new Timer(10000);
+                    timeWaster.Elapsed += new ElapsedEventHandler(wasteTime);
+
+
+                    while (userData.Funds < (item.Info.Ask * amount))
+                    {
+
+                        if (counter == 19)                   //have to waste time, not overload the server
+                        {
+                            timeWaster.Start();
+                            timeWaster.Stop();   //??? 
+                            counter = 0;
+                        }
+                         
+
+                            List<int> l = userData.Requests;
+
+                        // List<int> listOfUserRequests = ((MarketUserRequests) client.QueryUserRequests()).Requests;
+
+                        if (l.Count == 0)          //there are no more open requests
+                            break;
+
+                        client.SendCancelBuySellRequest(l[0]);
+                        HistoryLogger.WriteHistory( l[0] + "," + "Canceled");
+                        counter++;
+                        userData = client.SendQueryUserRequest();     //refresh data
+                        counter++;
+
+                    }
+
+                    if (userData.Funds >= item.Info.Ask * amount)
+                    {
+                        client.SendBuyRequest(item.Info.Ask, commodity, amount);
+                        HistoryLogger.WriteHistory(item.Info.Ask+   ","+ commodity    +","   + amount+  ","  + "Buy Request");
+                        counter++;
+                    }
+
+                }//bigIf
+
+            return;
+        }//AMA
     }
-
-    public static void AMA(int commodity, int desiredPrice, int amount)
-    {
-        MarketClientClass client = new MarketClientClass();
-        AllMarketRequest all = client.QueryAllMarketRequest();
-
-
-        foreach (ItemAskBid item in all.MarketInfo)
-            if (item.Id == commodity && item.Info.Ask <= desiredPrice)
-            {   //if item is the right commodity & right price
-
-                MarketUserData userData = client.SendQueryUserRequest();
-                while (userData.Funds < (item.Info.Ask * amount))
-                {
-                    List<int> l = userData.Requests;
-
-                    //maybe use new methood?
-                    // List<int> listOfUserRequests = ((MarketUserRequests) client.QueryUserRequests()).Requests;
-
-                    if (l == null)          //notSure. what if theres no more requests?
-                        break;
-
-                    client.SendCancelBuySellRequest(l[0]);
-                    userData = client.SendQueryUserRequest();     //refresh data
-                }
-
-                if (userData.Funds >= item.Info.Ask * amount)
-                    client.SendBuyRequest(item.Info.Ask, commodity, amount);
-
-            }//bigIf
-
-        return;
-    }//AMA
-
 }
+   
+
+    
+
+
+
+
+    
 
 
