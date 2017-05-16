ef44ac85ea3f79d638b67d26e0e2693efcc3bf75
רועי אש <ashr@post.bgu.ac.il>
2017-05-16 13:36:08 +0300

    small Changes Before Checkout

--- WPF_App/HistoryPage.xaml.cs
+++ WPF_App/HistoryPage.xaml.cs
@@ -12,17 +12,20 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DataTier.Loggers;
 
 namespace WPF_App
 {
-    /// <summary>
-    /// Interaction logic for HistoryPage.xaml
-    /// </summary>
-    public partial class HistoryPage : Page
-    {
+	/// <summary>
+	/// Interaction logic for HistoryPage.xaml
+	/// </summary>
+	public partial class HistoryPage : Page
+	{
+		public string[][] History { get; set; }
         public HistoryPage()
         {
             InitializeComponent();
+			History=HistoryLogger.ReadHistory();
 			DataContext=this;
         }
 	}
