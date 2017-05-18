b5f30359119988fa95bd269d436591cbd24f28f6
רועי אש <ashr@post.bgu.ac.il>
2017-05-18 20:16:42 +0300

    Changes

--- WPF_App/MainWindow.xaml.cs
+++ WPF_App/MainWindow.xaml.cs
@@ -19,7 +19,7 @@ namespace WPF_App
     {
 		private static readonly IMarketClient market = new MarketClientClass();
 		public static MarketUserData UserData { get; set; }
-		public ObservableCollection<Record>  History { get; set; }
+		public static ObservableCollection<Record>  History { get; set; }
 		public static ObservableCollection<MarketData> MarketData1 { get; set; }
 		public static ObservableCollection<MarketRequests> MarketRequests1 { get; set; }
 		private static DispatcherTimer timer = new DispatcherTimer()
@@ -91,14 +91,11 @@ namespace WPF_App
 				MessageBox.Show("Invalid Amount", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Error);
 			if (Commodity > 0 || Price > 0 || Amount > 0)
             {
-                //BuySellObject myObject = new BuySellObject(Operation.Buy, Commodity, Price, Amount);
                 IMarketClient market = new MarketClientClass();
                 MarketBuySell marketBuySell = market.SendBuyRequest(Price, Commodity, Amount);
                 if (marketBuySell.Error == null)
                 {
                     MessageBox.Show("Sucsess!! Your Buy request has been placed. your id is: " + marketBuySell.Id);
-                    //market.SendCancelBuySellRequest(marketBuySell.Id);
-                    //String output = Price+","+Commodity+","+Amount+","+"buy" ;
                     HistoryLogger.WriteHistory(marketBuySell.Id, "Buy", Commodity, Price, Amount);
 					Updater();
                 }
@@ -121,14 +118,11 @@ namespace WPF_App
                 MessageBox.Show("Invalid Amount", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Error);
             if (Commodity > 0 || Price > 0 || Amount > 0)
             {
-                //BuySellObject myObject = new BuySellObject(Operation.Buy, Commodity, Price, Amount);
                 IMarketClient market = new MarketClientClass();
                 MarketBuySell marketBuySell = market.SendSellRequest(Price, Commodity, Amount);
                 if (marketBuySell.Error == null)
                 {
                     MessageBox.Show("Sucsess!! Your Buy request has been placed. your id is: " + marketBuySell.Id);
-                    //market.SendCancelBuySellRequest(marketBuySell.Id);
-                    //String output = Price+","+Commodity+","+Amount+","+"buy" ;
                     HistoryLogger.WriteHistory(marketBuySell.Id, "Sell", Commodity, Price, Amount);
 					Updater();
                 }
@@ -198,6 +192,31 @@ namespace WPF_App
 				}
 			Updater();
 			Trace.WriteLine(int.Parse(((Button)sender).CommandParameter.ToString()));
+		}
+
+		private void BuyButton_Click_1(object sender, RoutedEventArgs e)
+		{
+			if (!(Int32.TryParse(SellCommodityField.Text, out int Commodity)))
+				MessageBox.Show("Invalid Commodity", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Error);
+			if (!(Int32.TryParse(SellPriceField.Text, out int Price)))
+				MessageBox.Show("Invalid Price", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Error);
+			if (!(Int32.TryParse(SellAmountField.Text, out int Amount)))
+				MessageBox.Show("Invalid Amount", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Error);
+			if (Commodity>0||Price>0||Amount>0)
+			{
+				IMarketClient market = new MarketClientClass();
+				MarketBuySell marketBuySell = market.SendBuyRequest(Price, Commodity, Amount);
+				if (marketBuySell.Error==null)
+				{
+					MessageBox.Show("Sucsess!! Your Buy request has been placed. your id is: "+marketBuySell.Id);
+					HistoryLogger.WriteHistory(marketBuySell.Id, "Sell", Commodity, Price, Amount);
+					Updater();
+				}
+				else
+					MessageBox.Show(marketBuySell.ToString());
+			}
+			else
+				MessageBox.Show("Invalid Input", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Information);
 		}
 	}
 }
