88a3a175067d90d3a05f94ca9cd1e9617724aecb
NoySaban <noysaba@post.bgu.ac.il>
2017-05-20 14:37:53 +0300

    starting unittest2

---
+++ MarketClientTest/UnitTest2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataTier;
+using DataTier.Utils;
+
+namespace DataTierTest
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        
+		[TestMethod]
+		public void TestQueryAllMarketRequest()
+		{
+			Assert.IsNull(new MarketClientClass().QueryUserRequests().Error);
+		}
+	}
+}
