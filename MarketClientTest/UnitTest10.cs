61b518c909f4eae4fdf49db513bbda3ab178e081
NoySaban <noysaba@post.bgu.ac.il>
2017-06-25 13:07:22 +0300

    Unit Tests 10&11 - check min max avg values

---
+++ MarketClientTest/UnitTest10.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataTier;
+using DataTier.Utils;
+using DataTier.DataEntries;
+using LogicTier;
+
+namespace MarketClientTest
+{
+    [TestClass]
+    public class UnitTest10
+    {
+        [TestMethod]
+        public void TestLegalSQLoutput()
+        {
+
+            int avg, min, max;
+            bool error = false;
+
+            try
+            {
+
+                Random rnd = new Random();
+                int rndCommodity1 = rnd.Next(0, 10);
+                int rndCommodity2 = rnd.Next(0, 10);
+                int rndCommodity3 = rnd.Next(0, 10);
+
+                //different cmds- check if the values returned are non negeative
+
+                avg = minMaxAvg.avgPrice(rndCommodity1);
+
+                min = minMaxAvg.minPrice(rndCommodity2);
+
+                max = minMaxAvg.maxPrice(rndCommodity3);
+
+                if (avg <= 0 | min <= 0 | max <= 0)
+                    error = true;
+            }
+
+            catch(Exception)
+            {
+                error = true;
+            }
+
+            Assert.IsFalse(error);
+
+        }
+    }
+}
---
+++ MarketClientTest/UnitTest11.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataTier;
+using DataTier.Utils;
+using DataTier.DataEntries;
+using LogicTier;
+
+namespace MarketClientTest
+{
+    [TestClass]
+    public class UnitTest11
+    {
+        [TestMethod]
+        public void TestMinMaxSQL()
+        {
+
+            int avg, min, max;
+            bool error = false;
+
+            try
+            {
+                Random rnd = new Random();
+                int rndCommodity = rnd.Next(0, 10);
+
+                //same cmd- check if the range is legal
+
+                avg = minMaxAvg.avgPrice(rndCommodity);
+
+                min = minMaxAvg.minPrice(rndCommodity);
+
+                max = minMaxAvg.maxPrice(rndCommodity);
+
+                if (min > avg | max < avg)
+                    error = true;
+            }
+
+            catch (Exception)
+            {
+                error = true;
+            }
+
+            Assert.IsFalse(error);
+
+
+        }
+    }
+}
