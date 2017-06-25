a5687cbae585320892ec0827a944cb7670d610db
NoySaban <noysaba@post.bgu.ac.il>
2017-06-25 12:46:39 +0300

    min,max avg SQL

--- ClassLibrary1/AMA.cs
+++ ClassLibrary1/AMA.cs
@@ -62,6 +62,9 @@ namespace LogicTier
                 return true;
         }
 
+
+
+
         public static void TimerOfAMA(bool b)
         {
             ResetBothTimers();              //not possible AMA auto & user requests
@@ -298,5 +301,102 @@ namespace LogicTier
                 counterServerCalls = 0;
             }
         }
+
+
+
+        public static int avgPrice(int cmd)
+        {
+            int avgPrice = -1;
+
+            using (SqlConnection myConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["historyConnectionString"].ConnectionString))
+            {
+                myConnection.Open();
+
+                SqlCommand myCommand = new SqlCommand("select Avg(price) AS AveragePrice from items where commodity='" + cmd + "')", myConnection);
+                SqlDataReader myDataReader = myCommand.ExecuteReader();
+                if (myDataReader.HasRows)
+                {
+                    myDataReader.Read();
+                    NotOverLoadServer();
+                    try
+                    {
+                        Trace.WriteLine(myDataReader[0].ToString());
+                        Trace.WriteLine(Double.Parse(myDataReader[0].ToString()));
+                        Double avgPriceDouble = Double.Parse(myDataReader[0].ToString());
+                        avgPrice = Convert.ToInt32(avgPriceDouble);
+                        return avgPrice;
+                    }
+                    catch
+                    {
+                        return avgPrice;
+                    }
+                }
+            }
+            return avgPrice;
+
+        }
+
+        public static int minPrice(int cmd)
+        {
+            int minPrice = -1;
+
+            using (SqlConnection myConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["historyConnectionString"].ConnectionString))
+            {
+                myConnection.Open();
+
+                SqlCommand myCommand = new SqlCommand("select MIN(Price) AS SmallestPrice items where commodity='" + cmd + "')", myConnection);
+                SqlDataReader myDataReader = myCommand.ExecuteReader();
+                if (myDataReader.HasRows)
+                {
+                    myDataReader.Read();
+                    NotOverLoadServer();
+                    try
+                    {
+                        Trace.WriteLine(myDataReader[0].ToString());
+                        Trace.WriteLine(Double.Parse(myDataReader[0].ToString()));
+                        Double minPriceDouble = Double.Parse(myDataReader[0].ToString());
+                        minPrice = Convert.ToInt32(minPriceDouble);
+                        return minPrice;
+                    }
+                    catch
+                    {
+                        return minPrice;
+                    }
+                }
+            }
+            return minPrice;
+        }
+
+        public static int maxPrice(int cmd)
+        {
+            int maxPrice = -1;
+
+            using (SqlConnection myConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["historyConnectionString"].ConnectionString))
+            {
+                myConnection.Open();
+
+                SqlCommand myCommand = new SqlCommand("select MAX(Price) AS LargestPrice from items where commodity='" + cmd + "')", myConnection);
+                SqlDataReader myDataReader = myCommand.ExecuteReader();
+                if (myDataReader.HasRows)
+                {
+                    myDataReader.Read();
+                    NotOverLoadServer();
+                    try
+                    {
+                        Trace.WriteLine(myDataReader[0].ToString());
+                        Trace.WriteLine(Double.Parse(myDataReader[0].ToString()));
+                        Double maxPriceDouble = Double.Parse(myDataReader[0].ToString());
+                        maxPrice = Convert.ToInt32(maxPriceDouble);
+                        return maxPrice;
+                    }
+                    catch
+                    {
+                        return maxPrice;
+                    }
+                }
+            }
+            return maxPrice;
+        }
+
     }//class
 }//namespace
